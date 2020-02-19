5cc33a7300c692d45a1abbb1d13d738b7f5f056f
Tom Nguyen <huuthong103@gmail.com>
2020-02-19 00:19:24 -0800

    finished check for possible move

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -12,11 +12,12 @@ grid = [
     [0, 0, 4, 2, 0, 0, 0, 0, 8]
 ]
 
+
 # using numpy to print a perfect matrix
-print(np.matrix(grid))
+# print(np.matrix(grid))
 
 # function to find the possible value of n for grid[y][x] = 0
-def possible(y,x,n):
+def possible_position(y, x, n):
     global grid
     # return False if there is an element at that grid[y][x] position
     # check if number n is exist in the row
@@ -29,5 +30,19 @@ def possible(y,x,n):
             return False
     # // means only take ground or round down to the whole number after the division
     # divide into smaller square (3x3)
-    x1 = (x//3)*3
-    y1 = (y//3)*3
+    x1 = (x // 3) * 3
+    y1 = (y // 3) * 3
+
+    # perform a check for each small square (3x3)
+    for i in range(0, 3):
+        for j in range(0, 3):
+            if grid[y1 + i][x1 + j] == n:
+                return False
+    return True
+
+
+# print(possible_position(4, 4, 1))
+
+
+def solve_puzzle():
+    global grid
