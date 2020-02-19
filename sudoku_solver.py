1f9e6723f81c54803e744c64af0d23ac080edacb
Tom Nguyen <huuthong103@gmail.com>
2020-02-18 23:40:43 -0800

    created the basic sudoku game using 2-dimensional array with some checking for next possible answer n

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,16 +1,33 @@
 import numpy as np
 
 grid = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    [0, 0, 0, 0, 0, 9, 0, 7, 0],
+    [0, 0, 0, 0, 8, 2, 0, 5, 0],
+    [3, 2, 7, 0, 0, 0, 0, 4, 0],
+    [0, 1, 6, 0, 4, 0, 0, 0, 0],
+    [0, 5, 0, 0, 0, 0, 3, 0, 0],
+    [0, 0, 0, 0, 9, 0, 7, 0, 0],
+    [0, 0, 0, 6, 0, 0, 0, 0, 5],
+    [8, 0, 2, 0, 0, 0, 0, 0, 0],
+    [0, 0, 4, 2, 0, 0, 0, 0, 8]
 ]
 
 # using numpy to print a perfect matrix
 print(np.matrix(grid))
+
+# function to find the possible value of n for grid[y][x] = 0
+def possible(y,x,n):
+    global grid
+    # return False if there is an element at that grid[y][x] position
+    # check if number n is exist in the row
+    for i in range(0, 9):
+        if grid[y][i] == n:
+            return False
+    # check if number n is exist in the column
+    for j in range(0, 9):
+        if grid[j][x] == n:
+            return False
+    # // means only take ground or round down to the whole number after the division
+    # divide into smaller square (3x3)
+    x1 = (x//3)*3
+    y1 = (y//3)*3
